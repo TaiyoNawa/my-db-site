1d13fb004c023947d765cfc1d7ebceea12ba157c
TaiyoNawa <t.nawa0222@gmail.com>
2024-11-03 16:32:54 +0900

    viewportのアドオンを追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,6 +14,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-viewport",
   ],
   framework: {
     name: "@storybook/nextjs",
