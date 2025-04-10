ddf0336d1d14af25de4107db2af74acbecbb9b3f
TaiyoNawa <t.nawa0222@gmail.com>
2025-04-11 03:58:29 +0900

    add: ESLintの設定

---
+++ .stylelintrc.ts
@@ -0,0 +1,16 @@
+// stylelint.config.js
+module.exports = {
+  extends: [
+    'stylelint-config-recess-order',
+    'stylelint-config-recommended-scss',
+  ],
+  rules: {
+    // ::before, ::afterのコロンを2つにする
+    'selector-pseudo-element-colon-notation': 'double',
+    // クラス名でアンパサンド（&）は禁止（&:hoverなどはOK）
+    'scss/selector-no-union-class-name': true,
+    // シングルクォーテーションに統一
+    'string-quotes': 'single',
+  },
+  ignoreFiles: ['**/node_modules/**', '**/*.ts', '**/*.tsx'],
+};
