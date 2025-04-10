ff820f915f98aebbd891d7ebb1a006f137674351
TaiyoNawa <t.nawa0222@gmail.com>
2025-04-11 04:02:55 +0900

    fix: 微修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,26 @@
-import type { AppProps } from "next/app";
-import { theme } from "../styles/theme";
-import { ChakraProvider, Box, Text } from "@chakra-ui/react";
-import { Layout } from "@/components/Layout";
-// import { GOOGLE_TAG_MANAGER_ID } from '@/config/constant';
+import { ChakraProvider } from '@chakra-ui/react';
+import { SWRConfig } from 'swr';
 
-// const options = {
-//   focusThrottleInterval: 60_000,
-//   revalidateIfStale: false,
-//   revalidateOnFocus: false,
-// };
+import { Layout } from '@/components/Layout';
+
+import { theme } from '../styles/theme';
+
+import type { AppProps } from 'next/app';
+
+const options = {
+  focusThrottleInterval: 60_000,
+  revalidateIfStale: false,
+  revalidateOnFocus: false,
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Layout>
-        <Component {...pageProps} />
-        <Box as="footer" bg="teal.500" color="white" p="4" mt="24">
-          <Text>&copy; 2024 My DB Site. All rights reserved.</Text>
-        </Box>
+        <SWRConfig value={options}>
+          <Component {...pageProps} />
+        </SWRConfig>
+        {/* <GoogleTagManagerLoader sec={2} /> */}
       </Layout>
     </ChakraProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-import { Header } from "@/components/Header";
 export default function Home() {
   return (
     <div className="style">
-      <Header />
+      人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは
       <h1>hello</h1>
     </div>
   );
