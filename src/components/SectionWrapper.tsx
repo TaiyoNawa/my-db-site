fffdc29292a9d68a25b5c347fcc3388b3740dab0
TaiyoNawa <t.nawa0222@gmail.com>
2025-04-16 05:47:04 +0900

    fix: SectionWrapperを修正

--- src/components/SectionWrapper.tsx
+++ src/components/SectionWrapper.tsx
@@ -1,26 +1,35 @@
 import { Box, BoxProps } from '@chakra-ui/react';
 import { FC, ReactNode } from 'react';
 
+import { SECTION_WRAPPER_PADDING } from '@/assets/data';
+
 type SectionWrapperProps = Pick<BoxProps, 'backgroundColor'> & {
   children: ReactNode;
   id?: string;
+  paddingTop?: string;
+  paddingBottom?: string;
 };
 
 export const SectionWrapper: FC<SectionWrapperProps> = ({
   children,
   backgroundColor,
   id,
+  paddingTop = SECTION_WRAPPER_PADDING,
+  paddingBottom = SECTION_WRAPPER_PADDING,
 }) => {
   return (
     <Box
       as="section"
-      py={{ base: '32px', md: '40px', lg: '52px' }}
-      px="3%"
-      mb="20px"
+      pt={paddingTop}
+      pb={paddingBottom}
       backgroundColor={backgroundColor}
       id={id}
     >
-      <Box w="full" maxW="1200px" mx="auto">
+      <Box
+        w="full"
+        maxW={{ base: '87%', sm: '366px', md: '692px', lg: '980px' }} //ここがSectionの大きさを決める！
+        mx="auto"
+      >
         {children}
       </Box>
     </Box>
