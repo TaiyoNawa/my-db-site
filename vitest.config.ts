1fd7246fe6df5303b55dcd6710a62644d8574188
TaiyoNawa <t.nawa0222@gmail.com>
2025-04-17 00:56:25 +0900

    fix: 微修正

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,16 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      '@': '/src',
     },
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: ["./setupTests.ts"],
+    environment: 'jsdom',
+    setupFiles: ['./setupTests.ts'],
   },
 });
