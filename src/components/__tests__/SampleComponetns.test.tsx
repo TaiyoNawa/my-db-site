305a2073f34b06c534337cc0c091443b3c48a9d4
TaiyoNawa <t.nawa0222@gmail.com>
2025-05-05 19:57:38 +0900

    fixデプロイ用に: 細かい修正

--- src/components/__tests__/SampleComponetns.test.tsx
+++ src/components/__tests__/SampleComponetns.test.tsx
@@ -1,22 +1,23 @@
-import { render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
-import { SampleText } from "../SampleComponent";
-import React from "react";
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { describe, it, expect } from 'vitest';
+
+import { SampleText } from '../SampleComponent';
 
 // const testProps1 = {
 //     summary:
 //       '株式会社ＦＵＴＵＲＥＷＯＯＤＳは、情報技術の進化を活用しつつ、営業シーンにおける課題を解決し、営業の生産性向上と人の介在価値の明確化に貢献することを目指している、営業・マーケティング領域のDX支援ツールやデータ解析など',
 //   };
 
-describe("SampleComponentのテスト", () => {
-  it("文字が表示される", () => {
+describe('SampleComponentのテスト', () => {
+  it('文字が表示される', () => {
     render(<SampleText />);
-    expect(screen.getByRole("heading")).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toBeInTheDocument();
   });
-  it("文字が正しく表示される", () => {
+  it('文字が正しく表示される', () => {
     render(<SampleText />);
-    expect(screen.getByRole("heading")).toHaveTextContent(
-      "これはStorybookのサンプルです"
+    expect(screen.getByRole('heading')).toHaveTextContent(
+      'これはStorybookのサンプルです'
     );
   });
 });
